af5f802234ffc9a7cc3cea4b964dee79c157f869
nirupam0007@gmail.com <nirupam0007@gmail.com>
2019-10-04 19:05:54 +0530

    Initial react scaffolding

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Berkadia Commertial Mortgage</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
